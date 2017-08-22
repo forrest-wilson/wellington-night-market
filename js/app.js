faf1de45356217f226cc9886d5a96a9e1343eb54
Forrest Wilson <forrest.wilson@me.com>
2017-08-22 22:32:54 +1200

    Initial idea
    Kind of messy but a majority of the main structure is there :)

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
+    $('.prevToggle').click(function() {
+        console.log("something");
+    });
 
+    $('.nextToggle').click(function() {
+        console.log("something else");
+    });
 });
