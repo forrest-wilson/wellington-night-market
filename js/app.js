b5bf62ed4d2ea3a355c1ad4237ad59d28f7186e1
Forrest Wilson <forrest.wilson@me.com>
2017-08-23 14:53:58 +1200

    removed line breaks in html

--- js/app.js
+++ js/app.js
@@ -47,49 +47,5 @@ $(document).ready(function() {
         } else {
             $navContainer.addClass('notAtTop');
         }
-
-        console.log($('#whatsOnSection').offset().top)
     });
 });
-$( document ).ready( function() {
-	//Google Maps JS
-	//Set Map
-	function initialize() {
-			var myLatlng = new google.maps.LatLng(-41.2926356,174.7760156,19.82);
-			var mapOptions = {
-				zoom: 20,
-				center: myLatlng,
-				mapTypeId: google.maps.MapTypeId.ROADMAP
-			}
-
-		var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-		//Callout Content
-		var contentString = 'Some address here..';
-		//Set window width + content
-		var infowindow = new google.maps.InfoWindow({
-			content: contentString,
-			maxWidth: 500
-		});
-
-		//Add Marker
-		var marker = new google.maps.Marker({
-			position: myLatlng,
-			map: map
-		});
-
-		google.maps.event.addListener(marker, 'click', function() {
-			infowindow.open(map,marker);
-		});
-
-		//Resize Function
-		google.maps.event.addDomListener(window, "resize", function() {
-			var center = map.getCenter();
-			google.maps.event.trigger(map, "resize");
-			map.setCenter(center);
-		});
-	}
-
-	google.maps.event.addDomListener(window, 'load', initialize);
-
-});
-
