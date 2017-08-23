d465abd77ecaac52f5ef4fd38d77e5ae1ff80cc0
Forrest Wilson <forrest.wilson@me.com>
2017-08-23 14:59:23 +1200

    Revert "changed $scroll variable to $scrollPos"
    This reverts commit 2b7da98bd1fae603a27a167187b847af579c11cc.

--- js/app.js
+++ js/app.js
@@ -36,18 +36,16 @@ $(document).ready(function() {
     // Call methods when scrolling
     $(document).scroll(function() {
         // Variable declaration to help with browser caching (decrease load times)
-        var $scrollPos = $(document).scrollTop();
+        var $scroll = $(document).scrollTop();
         var $navContainer = $('#navContainer')
 
         // Adds and removes the notAtTop class depending on scroll position
         // change the number value in the if() statement to change the point
         // at which the function runs
-        if ($scrollPos < 575) {
+        if ($scroll < 575) {
             $navContainer.removeClass('notAtTop');
         } else {
             $navContainer.addClass('notAtTop');
         }
-
-
     });
 });
