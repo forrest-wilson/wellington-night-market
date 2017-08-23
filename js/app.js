7990a095a2724288fa22323599914f9ab1df253b
Forrest Wilson <forrest.wilson@me.com>
2017-08-23 13:27:34 +1200

    added nav bar opacity change when scrolling down the page

--- js/app.js
+++ js/app.js
@@ -6,4 +6,15 @@ $(document).ready(function() {
     $('.nextToggle').click(function() {
         console.log("something else");
     });
+
+    $(document).scroll(function() {
+        var $scroll = $(document).scrollTop();
+        var $navContainer = $('#navContainer')
+
+        if ($scroll > 575) {
+            $navContainer.addClass('notAtTop');
+        } else {
+            $navContainer.removeClass('notAtTop');
+        }
+    });
 });
