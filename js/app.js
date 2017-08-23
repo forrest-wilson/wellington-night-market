095d914d0b1dd7fc9f829ab570e8b5f0ebcb8438
Forrest Wilson <forrest.wilson@me.com>
2017-08-23 13:40:31 +1200

    added ids to nav items and added scroll to first nav item

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
+    // Variable declarations
+    var $scrollTime = 600;
+
+    $('#whatsOnNavItem').click(function() {
+        $('html body').animate({
+            scrollTop: $('#whatsOnHeader').offset().top - 75
+        }, $scrollTime);
+    });
+
     $('.prevToggle').click(function() {
         console.log("something");
     });
@@ -7,6 +16,7 @@ $(document).ready(function() {
         console.log("something else");
     });
 
+    // Call methods when scrolling
     $(document).scroll(function() {
         // Variable declaration to help with browser caching (decrease load times)
         var $scroll = $(document).scrollTop();
@@ -15,7 +25,7 @@ $(document).ready(function() {
         // Adds and removes the notAtTop class depending on scroll position
         // change the number value in the if() statement to change the point
         // at which the function runs
-        if ($scroll > 575) {
+        if ($scroll >= 575) {
             $navContainer.addClass('notAtTop');
         } else {
             $navContainer.removeClass('notAtTop');
