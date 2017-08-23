ef4a31f28fd89df8bc4a06a57fb4bcfbf00b5c23
Forrest Wilson <forrest.wilson@me.com>
2017-08-23 13:30:37 +1200

    added comments to the app.js file

--- js/app.js
+++ js/app.js
@@ -8,9 +8,13 @@ $(document).ready(function() {
     });
 
     $(document).scroll(function() {
+        // Variable declaration to help with browser caching (decrease load times)
         var $scroll = $(document).scrollTop();
         var $navContainer = $('#navContainer')
 
+        // Adds and removes the notAtTop class depending on scroll position
+        // change the number value in the if() statement to change the point
+        // at which the function runs
         if ($scroll > 575) {
             $navContainer.addClass('notAtTop');
         } else {
